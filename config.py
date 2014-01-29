514ba6e842ff3efaf46b5db17358dcfc79de03ca
Nikita Pekin <nikitapekin@gmail.com>
2014-01-29 07:26:05 -0500

    Begin adding additional mods

--- config.py
+++ config.py
@@ -11,5 +11,25 @@ MODS = {
         'name': 'REI\'s Minimap',
         'version': '3.4_03beta',
         'url': 'https://dl.dropboxusercontent.com/u/34787499/minecraft/1.7.2/%5B1.7.2%5DReiMinimap_v3.4_03beta.zip'
+    },
+    'status_effect_hud': {
+        'name': 'Status Effect HUD',
+        'version': '1.21',
+        'url': 'http://dl.dropboxusercontent.com/u/20748481/MC/StatusEffectHUD/latest/%5B1.7.2%5DStatusEffectHUD-client-1.21(1.7.2).jar'
+    },
+    'armor_status_hud': {
+        'name': 'Armor Status HUD',
+        'version': '1.18',
+        'url': 'http://dl.dropboxusercontent.com/u/20748481/MC/ArmorStatusHUD/latest/%5B1.7.2%5DArmorStatusHUD-client-1.18(1.7.2).jar'
+    },
+    'bspkrsCore': {
+        'name': 'bspkrsCore',
+        'version': '6.0',
+        'url': 'http://dl.dropboxusercontent.com/u/20748481/MC/bspkrsCore/latest/%5B1.7.2%5DbspkrsCore-universal-6.0%281.7.2%29.jar'
+    },
+    'fml': {
+        'name': 'FML',
+        'version': '7.2.109.863',
+        'url': 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.2-10.12.0.1019/forge-1.7.2-10.12.0.1019-universal.jar'
     }
 }
