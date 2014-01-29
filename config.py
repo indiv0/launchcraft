38cc8f686b1264754021d2a497adea651da10afa
Nikita Pekin <nikitapekin@gmail.com>
2014-01-29 07:12:52 -0500

    Update optifine version

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ VERSION = '1.7.2'
 MODS = {
     'optifine': {
         'name': 'Optifine',
-        'version': '1.7.2_HD_U_A9',
-        'url': 'http://optifine.net/download.php?f=OptiFine_1.7.2_HD_U_A9.jar'
+        'version': '1.7.2_HD_U_C2',
+        'url': 'http://optifine.net/download.php?f=OptiFine_1.7.2_HD_U_C2.jar'
     },
     'reis': {
         'name': 'REI\'s Minimap',
