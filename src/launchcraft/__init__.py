8a963405a02384658d4145c1c29c0a4d7b0f3a93
Nikita Pekin <contact@nikitapek.in>
2015-04-10 01:23:15 -0400

    Move util module to utils

--- src/launchcraft/__init__.py
+++ src/launchcraft/__init__.py
@@ -13,7 +13,7 @@ import subprocess
 
 import json
 
-import util
+import utils
 
 try:
     VERSION = __import__('pkg_resources') \
@@ -41,27 +41,27 @@ SHADERPACK_DIR = os.path.join(MINECRAFT_DIR, 'shaderpacks')
 
 
 def main():
-    if util.DATA['version'] != VERSION:
-        print('Your version of Launchcraft ({}) does not match the minimum version of Launchcraft ({}). Please update.'.format(VERSION, util.DATA['version']))
-        util.exit()
+    if utils.DATA['version'] != VERSION:
+        print('Your version of Launchcraft ({}) does not match the minimum version of Launchcraft ({}). Please update.'.format(VERSION, utils.DATA['version']))
+        utils.exit()
 
     print('This script will ask you yes or no questions.')
     print('Any answers in square brackets (e.g. [1.7.10]), or that are capitalized (e.g. [Y/n]) are the default answers, and will be selected when you press enter.')
-    util.print_separator()
+    utils.print_separator()
 
     version = raw_input('Which version of Minecraft would you like to use? [1.7.10]: ').lower()
     if version == '':
         version = '1.7.10'
 
-    if version not in util.DATA['versions']:
+    if version not in utils.DATA['versions']:
         print("Invalid version selected.")
-        util.exit()
+        utils.exit()
 
-    util.MODS = util.DATA['versions'][version]
+    utils.MODS = utils.DATA['versions'][version]
 
     JAR_DIR = os.path.join(VERSIONS_DIR, version)
 
-    FORGE_VERSION = '{}-Forge{}'.format(version, util.MODS['mods']['forge']['version'])
+    FORGE_VERSION = '{}-Forge{}'.format(version, utils.MODS['mods']['forge']['version'])
     FORGE_DIR = os.path.join(VERSIONS_DIR, FORGE_VERSION)
 
     print('Entering directory "{}".'.format(MINECRAFT_DIR))
@@ -69,8 +69,8 @@ def main():
         os.chdir(MINECRAFT_DIR)
     except:
         print('Failed to enter minecraft directory, please install minecraft first.')
-        util.exit()
-    util.print_separator()
+        utils.exit()
+    utils.print_separator()
 
     # Set the directory to which the custom profile will be installed.
     profile_name = raw_input('What would you like to call the profile being created? [launchcraft]: ').lower()
@@ -89,10 +89,10 @@ def main():
         else:
             print(ex)
             print('Failed to remove old profile directory, exiting...')
-            util.exit()
-    util.print_separator()
+            utils.exit()
+    utils.print_separator()
 
-    forge = util.query_yes_no('Would you like to use Forge?', default='no')
+    forge = utils.query_yes_no('Would you like to use Forge?', default='no')
     if forge:
         if os.path.exists(FORGE_DIR):
             print('The required Forge version has been detected on your system.')
@@ -101,8 +101,8 @@ def main():
             print('The required Forge version has not been detected on your system.')
             message = 'install'
         # Ask the user whether or not they need Forge.
-        if util.query_yes_no('Do you need to {} Forge?'.format(message), default='no'):
-            forge = util.MODS['mods']['forge']
+        if utils.query_yes_no('Do you need to {} Forge?'.format(message), default='no'):
+            forge = utils.MODS['mods']['forge']
             name = forge['name']
             version = forge['version']
             jarName = 'forge.jar'
@@ -112,11 +112,11 @@ def main():
 
             # Download the Forge installer.
             print('Downloading {} version {}'.format(name, version))
-            util.downloadFile(forge['url'], jarName)
+            utils.downloadFile(forge['url'], jarName)
 
             if sys.platform == 'win32' or sys.platform == 'cygwin':
                 print('You must now run the {} that has been downloaded to your Launchcraft directory.'.format(jarName))
-                util.exit()
+                utils.exit()
             else:
                 # Run the installer so the user can install Forge.
                 print('You will now be asked to install Forge version {}.'.format(version))
@@ -124,17 +124,17 @@ def main():
                     subprocess.call('java -jar {}'.format(jarName), shell=True, stdout=devnull)
 
                 os.remove(jarName)
-    util.print_separator()
+    utils.print_separator()
 
     JAR_FILE = os.path.join(PROFILE_DIR, '{}.jar'.format(profile_name))
     JSON_FILE = os.path.join(PROFILE_DIR, '{}.json'.format(profile_name))
 
     if forge:
-        print('Using Forge {} as the base for the profile'.format(util.MODS['mods']['forge']['version']))
+        print('Using Forge {} as the base for the profile'.format(utils.MODS['mods']['forge']['version']))
         if not os.path.exists(MOD_DIR):
             os.makedirs(MOD_DIR)
 
-        util.INSTALLED_MODS.append('forge')
+        utils.INSTALLED_MODS.append('forge')
         JAR_DIR = FORGE_DIR
         print('Creating new profile directory.')
         shutil.copytree(FORGE_DIR, PROFILE_DIR)
@@ -153,14 +153,14 @@ def main():
         except OSError as ex:
             print(ex)
             print('Failed to create new profile directory, exiting...')
-            util.exit()
+            utils.exit()
 
         print('Entering newly created profile directory.')
         os.chdir(PROFILE_DIR)
 
         print('Downloading "{0}.jar" and "{0}.json".'.format(version))
-        util.downloadFile('https://s3.amazonaws.com/Minecraft.Download/versions/{0}/{0}.jar'.format(version), '{}.jar'.format(profile_name))
-        util.downloadFile('https://s3.amazonaws.com/Minecraft.Download/versions/{0}/{0}.json'.format(version), '{}.json'.format(version))
+        utils.downloadFile('https://s3.amazonaws.com/Minecraft.Download/versions/{0}/{0}.jar'.format(version), '{}.jar'.format(profile_name))
+        utils.downloadFile('https://s3.amazonaws.com/Minecraft.Download/versions/{0}/{0}.json'.format(version), '{}.json'.format(version))
         SOURCE_JSON_FILE = '{}.json'.format(version)
 
     print('Creating "{}.json".'.format(profile_name))
@@ -172,31 +172,31 @@ def main():
 
     print('Deleting "{}".'.format(SOURCE_JSON_FILE))
     os.remove(SOURCE_JSON_FILE)
-    util.print_separator()
+    utils.print_separator()
 
-    if util.query_yes_no('Do you want to install mods?', default='no'):
+    if utils.query_yes_no('Do you want to install mods?', default='no'):
         print('Which mods would you like to install?')
-        toInstall = util.printAskOptions(util.MODS['mods'])
+        toInstall = utils.printAskOptions(utils.MODS['mods'])
         print('Installing mods.')
         print('')
         for mod in toInstall:
-            modData = util.MODS['mods'][mod]
+            modData = utils.MODS['mods'][mod]
             skip = False
 
-            conflicts = [i for i in modData['conflicts'] if i in util.INSTALLED_MODS]
+            conflicts = [i for i in modData['conflicts'] if i in utils.INSTALLED_MODS]
 
             if mod == 'forge':
                 continue
 
             # Do not install forge-dependant mods if Forge is not installed.
-            if 'forge' in modData['deps'] and 'forge' not in util.INSTALLED_MODS:
+            if 'forge' in modData['deps'] and 'forge' not in utils.INSTALLED_MODS:
                 print('Skipping {} due to missing Forge'.format(modData['name']))
                 skip = True
             # Skip conflicting mods
             elif conflicts:
                 conflicting_mods = ""
                 for i in conflicts:
-                    conflicting_mods += util.MODS['mods'][i]['name'] + ", "
+                    conflicting_mods += utils.MODS['mods'][i]['name'] + ", "
                 print('Skipping {} because it conflicts with {}'.format(modData['name'], conflicting_mods[:-2]))
                 skip = True
 
@@ -204,42 +204,42 @@ def main():
                 print('')
                 continue
 
-            util.installDep(mod, JAR_FILE)
+            utils.installDep(mod, JAR_FILE)
             print('')
 
-    util.removeMETAINF(JAR_FILE)
-    util.print_separator()
+    utils.removeMETAINF(JAR_FILE)
+    utils.print_separator()
 
-    if util.query_yes_no('Do you want to install texture packs?', default='no'):
+    if utils.query_yes_no('Do you want to install texture packs?', default='no'):
         if not os.path.exists(RESOURCEPACK_DIR):
             os.makedirs(RESOURCEPACK_DIR)
         print("What texture packs would you like to install?")
-        toInstall = util.printAskOptions(util.MODS['resourcepacks'])
+        toInstall = utils.printAskOptions(utils.MODS['resourcepacks'])
         print('Installing resourcepacks.')
         print('')
         for pack in toInstall:
-            packData = util.MODS['resourcepacks'][pack]
+            packData = utils.MODS['resourcepacks'][pack]
 
-            util.installResourcePack(pack)
+            utils.installResourcePack(pack)
             print('')
-    util.print_separator()
+    utils.print_separator()
 
-    if util.query_yes_no('Do you want to install shader packs?', default='no'):
+    if utils.query_yes_no('Do you want to install shader packs?', default='no'):
         if not os.path.exists(SHADERPACK_DIR):
             os.makedirs(SHADERPACK_DIR)
         print("What shader packs would you like to install?")
-        toInstall = util.printAskOptions(util.MODS['shaderpacks'])
+        toInstall = utils.printAskOptions(utils.MODS['shaderpacks'])
         print('Installing shaderpacks.')
         print('')
         for pack in toInstall:
-            packData = util.MODS['shaderpacks'][pack]
+            packData = utils.MODS['shaderpacks'][pack]
 
-            util.installShaderPack(pack)
+            utils.installShaderPack(pack)
             print('')
-    util.print_separator()
+    utils.print_separator()
 
     print('Completed successfully!')
-    util.exit()
+    utils.exit()
 
     try:
         input('Press any key to exit...')
